8f04019f8b0cbf1d03cfc91f110c910616239088
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-05 20:17:26 +0900

    feat(SeatType): 좌석 타입 구현

---
+++ src/main/java/domain/seat/SeatType.java
@@ -0,0 +1,17 @@
+package domain.seat;
+
+public enum SeatType {
+    S(18_000),
+    A(15_000),
+    B(12_000);
+
+    private final int price;
+
+    SeatType(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
