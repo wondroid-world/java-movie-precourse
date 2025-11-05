c7646788fbd0168e1ce72ce189d158ebd11fe09e
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-05 20:17:38 +0900

    feat(Seat): 좌석 구현

---
+++ src/main/java/domain/seat/Seat.java
@@ -0,0 +1,37 @@
+package domain.seat;
+
+public class Seat {
+    private final SeatNumber seatNumber;
+    private final SeatType seatType;
+    private Boolean isBook;
+
+    public Seat(SeatNumber seatNumber, SeatType seatType, Boolean isBook) {
+        this.seatNumber = seatNumber;
+        this.seatType = seatType;
+        this.isBook = isBook;
+    }
+
+    public Seat(SeatNumber seatNumber, SeatType seatType) {
+        this(seatNumber, seatType, false);
+    }
+
+    public Seat(char row, int column, SeatType seatType) {
+        this(new SeatNumber(new Row(row), new Column(column)), seatType, false);
+    }
+
+    public Boolean getBook() {
+        return isBook;
+    }
+
+    public void book() {
+        isBook = true;
+    }
+
+    public void cancel() {
+        isBook = false;
+    }
+
+    public int getPrice() {
+        return seatType.getPrice();
+    }
+}
---
+++ src/test/java/domain/seat/SeatTest.java
@@ -0,0 +1,59 @@
+package domain.seat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SeatTest {
+    private Seat seat;
+
+    @BeforeEach
+    void setup() {
+        seat = new Seat('A', 1, SeatType.A);
+    }
+
+    @Test
+    void 좌석을_처음_잡았을때_빈_좌석이다(){
+        // given, when
+        Boolean result = seat.getBook();
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 좌석을_예약하면_true를_반환한다() {
+        // given, when
+        seat.book();
+        Boolean result = seat.getBook();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 좌석을_예약을_취소하면_false를_반환한다() {
+        // given
+        seat.book();
+        seat.cancel();
+
+        // when
+        Boolean result = seat.getBook();
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 좌석의_가격을_알_수_있다() {
+        // given
+        int expected = SeatType.A.getPrice();
+
+        // when
+        int result = seat.getPrice();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+}
