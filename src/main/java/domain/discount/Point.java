61168e7a3a4ce8a95764967897b6d0b51e4096c3
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-06 14:25:09 +0900

    feat(Point) : 포인트 객체 구현

---
+++ src/main/java/domain/discount/Point.java
@@ -0,0 +1,17 @@
+package domain.discount;
+
+public record Point(int value) {
+    public static final String ERROR_POINT_NO_NEGATIVE = "포인트는 마이너스 값이 올 수 없습니다.";
+
+    public Point {
+        if (value < 0) throw new IllegalArgumentException(ERROR_POINT_NO_NEGATIVE);
+    }
+
+    public Point plus(Point point) {
+        return new Point(this.value + point.value);
+    }
+
+    public Point minus(Point point) {
+        return new Point(this.value - point.value);
+    }
+}
---
+++ src/test/java/domain/discount/PointTest.java
@@ -0,0 +1,50 @@
+package domain.discount;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PointTest {
+    private Point point;
+
+    @BeforeEach
+    void setup() {
+        point = new Point(100);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -3})
+    void 포인트는_마이너스_값이_올_수_없다(int value) {
+        assertThrows(IllegalArgumentException.class, () -> new Point(value));
+    }
+
+    @Test
+    void 포인트는_더할_수_있다() {
+        // given
+        Point point1 = new Point(100);
+        Point point2 = new Point(100);
+
+        // when
+        Point result = point1.plus(point2);
+
+        // then
+        assertThat(result.value()).isEqualTo(200);
+    }
+
+    @Test
+    void 포인트는_뺄_수_있다() {
+        // given
+        Point point1 = new Point(100);
+        Point point2 = new Point(100);
+
+        // when
+        Point result = point1.minus(point2);
+
+        // then
+        assertThat(result.value()).isEqualTo(0);
+    }
+}
