5fd801b4c0bc13e05ca961f81761ca4a3d422d3a
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-06 14:07:22 +0900

    refactor(Column) : 상수화

--- src/main/java/domain/seat/Column.java
+++ src/main/java/domain/seat/Column.java
@@ -2,12 +2,12 @@ package domain.seat;
 
 public record Column(int value) {
     public Column {
-        if (value < START_VALUE || value > END_VALUE) {
+        if (value < START_COLUMN || value > END_COLUMN) {
             throw new IllegalArgumentException(ERROR_SEAT_COLUMN);
         }
     }
 
-    private static final int START_VALUE = 1;
-    private static final int END_VALUE = 45;
+    private static final int START_COLUMN = 1;
+    private static final int END_COLUMN = 45;
     public static final String ERROR_SEAT_COLUMN = "열은 1 ~ 45 사이의 숫자만 올 수 있습니다.";
 }
