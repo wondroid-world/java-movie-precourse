5bf8a221a7fbbc0302fd8485386c6df1690c1590
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-06 14:07:05 +0900

    refactor(Row) : 상수화

--- src/main/java/domain/seat/Row.java
+++ src/main/java/domain/seat/Row.java
@@ -2,10 +2,12 @@ package domain.seat;
 
 public record Row(char value) {
     public Row {
-        if (value < 'A' || value > 'P') {
+        if (value < START_ROW || value > END_ROW) {
             throw new IllegalArgumentException(ERROR_SEAT_ROW);
         }
     }
 
+    private static final char START_ROW = 'A';
+    private static final char END_ROW = 'P';
     public static final String ERROR_SEAT_ROW = "행은 A ~ P 사이의 알파벳 대문자만 올 수 있습니다.";
 }
