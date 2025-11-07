85989fb151e8ad529f9392f5556c59774c02f23d
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-07 14:10:20 +0900

    chore(PointTest) : 패키지 구조 변경

--- src/test/java/domain/PointTest.java
+++ src/test/java/domain/PointTest.java
@@ -1,6 +1,5 @@
-package domain.discount;
+package domain;
 
-import domain.Point;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
