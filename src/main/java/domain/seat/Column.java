29f4869cd30401c705af5787ae3198934ffcdb5f
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-07 16:47:01 +0900

    refactor(Column, Row) : 자바 문법에 맞게 순서 변환

--- src/main/java/domain/seat/Column.java
+++ src/main/java/domain/seat/Column.java
@@ -1,13 +1,13 @@
 package domain.seat;
 
 public record Column(int value) {
+    private static final int START_COLUMN = 1;
+    private static final int END_COLUMN = 45;
+    private static final String ERROR_SEAT_COLUMN = "열은 1 ~ 45 사이의 숫자만 올 수 있습니다.";
+
     public Column {
         if (value < START_COLUMN || value > END_COLUMN) {
             throw new IllegalArgumentException(ERROR_SEAT_COLUMN);
         }
     }
-
-    private static final int START_COLUMN = 1;
-    private static final int END_COLUMN = 45;
-    public static final String ERROR_SEAT_COLUMN = "열은 1 ~ 45 사이의 숫자만 올 수 있습니다.";
 }
--- src/main/java/domain/seat/Row.java
+++ src/main/java/domain/seat/Row.java
@@ -1,13 +1,13 @@
 package domain.seat;
 
 public record Row(char value) {
+    private static final char START_ROW = 'A';
+    private static final char END_ROW = 'P';
+    private static final String ERROR_SEAT_ROW = "행은 A ~ P 사이의 알파벳 대문자만 올 수 있습니다.";
+
     public Row {
         if (value < START_ROW || value > END_ROW) {
             throw new IllegalArgumentException(ERROR_SEAT_ROW);
         }
     }
-
-    private static final char START_ROW = 'A';
-    private static final char END_ROW = 'P';
-    public static final String ERROR_SEAT_ROW = "행은 A ~ P 사이의 알파벳 대문자만 올 수 있습니다.";
 }
