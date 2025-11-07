8e95c7191335f397815bc474708367e1c6c196fd
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-11-07 18:02:05 +0900

    feat(Theaters) : 여러 Theater를 관리할 수 있는 Theaters 클래스 추가

---
+++ src/main/java/domain/theater/Theaters.java
@@ -0,0 +1,22 @@
+package domain.theater;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Theaters {
+    private final List<Theater> theaters = new ArrayList<>();
+
+    public void add(Theater newTheater) {
+        if (!theaters.contains(newTheater)) {
+            theaters.add(newTheater);
+        }
+    }
+
+    public void remove(Theater theater) {
+        theaters.remove(theater);
+    }
+
+    public List<Theater> getAllTheaters() {
+        return theaters;
+    }
+}
